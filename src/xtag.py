67accbf5ec02564c150551ded0574bd4f2fb4e75
Wilke Schwiedop <w.schwiedop@web.de>
2011-04-29 15:52:26 +0200

    YACEC - Yet Another 'Changed Everything' Commit

--- src/xtag.py
+++ src/xtag.py
@@ -1,12 +1,10 @@
 import os, os.path as path
-import sys
 
 REPO_DIR = ".xtag"
 __basedir = None
 
-class XTagException(Exception):
-	def __init__(*msg):
-		Exception.__init__(msg)
+class XTagError(Exception):
+	pass
 
 def basedir():
 	""" Return path to .xtag/ parent-dir """
@@ -18,8 +16,6 @@ def basedir():
 				__basedir = basedir
 				break
 			basedir = path.dirname(basedir)
-		else:
-			raise XTagException()
 	return __basedir
 
 def repodir():
@@ -42,59 +38,66 @@ def init():
 	if repodir() == None:
 		os.mkdir(REPO_DIR, 0o744)
 	else:
-		raise XTagException("Is an xtag repository already")
+		raise XTagError("Is an xtag repository already")
 
-def modify_tags(modify, file, tags):
+def modify_tags(modify, files, tags):
 	if repodir() == None:
-		raise XTagException("Not an xtag repository")
-	if not path.exists(file):
-		raise XTagException("TODO: if not path.exists(file)")
-	if not len(tags) > 0:
-		raise XTagException("TODO: if not len(tags) > 0")
+		raise XTagError("Not an xtag repository")
 
-	if path.isfile(file):
-		file = path.abspath(file)
-		print(modify.__name__, path.basename(file), "tags:", *tags, sep='\t')
-		modify(file, tags)
-	elif path.isdir(file):
-		if True: # TODO: option --recursive
-			for f in os.listdir(file):
-				modify_tags(modify, path.join(file, f), tags)
-		else:
-			raise XTagException(file, "is a directory")
+	for file in files:
+		if path.isfile(file):
+			#file = path.abspath(file)
+			print(modify.__name__, path.basename(file), "tags:", *tags, sep='\t')
+		elif path.isdir(file):
+			if True: # TODO: option --recursive
+				for f in os.listdir(file):
+					modify_tags(modify, path.join(file, f), tags)
+			else:
+				raise XTagError(file, "is a directory")
+	modify(files, tags)
 
-def add_tags(file, tags):
-	""" Add tags to file """
+def add_tags(files, tags):
+	""" Add tags to files """
+	files_and_hashes = [(file, taghash(file)) for file in files]
 	for tag in tags:
-		tag = path.join(repodir(), tag)
-		tagfile = path.join(tag, taghash(file))
-		if not path.exists(tag):
-			os.mkdir(tag)
-		if not path.exists(tagfile):
-			os.symlink(file, tagfile)
+		tagdir = path.join(repodir(), tag)
+		if not path.exists(tagdir):
+			os.mkdir(tagdir)
+		for file, hash in files_and_hashes:
+			relfile = path.relpath(file, tagdir)
+			tagfile = path.join(tagdir, hash)
+			try:
+				os.symlink(relfile, tagfile)
+			except:
+				pass
 
-def remove_tags(file, tags):
-	""" Remove tags from file """
+def remove_tags(files, tags):
+	""" Remove tags from files """
+	hashes = [taghash(file) for file in files]
 	for tag in tags:
-		tag = path.join(repodir(), tag)
-		tagfile = path.join(tag, taghash(file))
-		if path.exists(tagfile):
+		tagdir = path.join(repodir(), tag)
+		for hash in hashes:
+			tagfile = path.join(tagdir, hash)
 			os.unlink(tagfile)
-		if os.listdir(tag) == []:
-			os.rmdir(tag)
+		try:
+			os.rmdir(tagdir)
+		except:
+			pass
 
-def set_tags(file, tags):
-	""" Set tags of file """
-	current_tags = get_tags_by_file(file)
-	add_tags(file, [tag for tag in tags if tag not in current_tags])
-	remove_tags(file, [tag for tag in current_tags if tag not in tags])
+def set_tags(files, tags):
+	""" Set tags of files """
+	for file in files:
+		current_tags = get_tags_by_file(file)
+		add_tags(file, [tag for tag in tags if tag not in current_tags])
+		remove_tags(file, [tag for tag in current_tags if tag not in tags])
 
 def list(tags):
-	""" List files having all passed tags """
-	files = get_files_by_tag(tags[0])
+	""" List tagfiles having all passed tags """
+	tagfiles = get_files_by_tag(tags[0])
 	for tag in tags[1:]:
-		files = [tagfile for tagfile in files if tagfile in get_files_by_tag(tag)] # wtf :D
-	print(*[os.readlink(path.join(repodir(), tags[0], tagfile)) for tagfile in files], sep='\n')
+		tagfiles = [tagfile for tagfile in tagfiles if tagfile in get_files_by_tag(tag)]
+	for tagfile in tagfiles:
+		yield(path.abspath(os.readlink(path.join(repodir(), tags[0], tagfile))))
 
 def orphans():
 	""" Lists orphaned tags """
@@ -103,4 +106,4 @@ def orphans():
 			file = os.readlink(path.join(repodir(), tag, tagfile))
 			# broken symlink or different file (taghash differs)
 			if not path.isfile(file) or taghash(file) != tagfile:
-				print(path.join(tag, tagfile))
+				yield(path.join(repodir(), tag, tagfile))
--- src/xtag_cli.py
+++ src/xtag_cli.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 from xtag import *
+import sys
 
 def die(*msg):
-	err(*msg)
+	print(*msg, file=sys.stderr)
 	sys.exit(1)
 
 commands = {
 	"init"        : lambda args: init(),
-	"tag"         : lambda args: tag_files(args[0], args[1:]),
-	"untag"       : lambda args: untag_files(args[0], args[1:]),
-	"add-tags"    : lambda args: modify_tags(add_tags, args[0], args[1:]),
-	"remove-tags" : lambda args: modify_tags(remove_tags, args[0], args[1:]),
-	"set-tags"    : lambda args: modify_tags(set_tags, args[0], args[1:]),
-	"list"        : lambda args: list(args),
-	"orphans"     : lambda args: orphans(),
+	"tag"         : lambda args: modify_tags(add_tags, args[1:], args[:1]),
+	"untag"       : lambda args: modify_tags(remove_tags, args[1:], args[:1]),
+	"add-tags"    : lambda args: modify_tags(add_tags, args[:1], args[1:]),
+	"remove-tags" : lambda args: modify_tags(remove_tags, args[:1], args[1:]),
+	"set-tags"    : lambda args: modify_tags(set_tags, args[:1], args[1:]),
+	"list"        : lambda args: print(*list(args), sep='\n'),
+	"orphans"     : lambda args: print(*orphans(), sep='\n'),
 	"help"        : lambda args: print(*commands.keys(), sep='\n'),
 }
 
@@ -31,5 +32,5 @@ elif len(cmds) > 1:
 
 try:
 	commands[cmds[0]](sys.argv)
-except e:
-	die(e.msg)
+except Exception as e:
+	die(*e.args)
